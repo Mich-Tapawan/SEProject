ee595d4851d84cc4972226e320b10fd6d09ec9fb
Mich-Tapawan <mik.tapawan@gmail.com>
2024-11-19 20:46:18 +0800

    Added initial request body

--- server/server.ts
+++ server/server.ts
@@ -3,10 +3,6 @@ import cors from "cors";
 import { MongoClient, ServerApiVersion } from "mongodb";
 import "dotenv/config";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
 const uri = process.env.MONGODB_URI;
 
 if (!uri) {
@@ -38,6 +34,11 @@ async function run() {
 }
 run().catch(console.dir);
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Services API endpoints
 const netflixRoutes = require("./services/netflix");
 const spotifyRoutes = require("./services/spotify");
 const youtubeRoutes = require("./services/youtube");
@@ -51,5 +52,33 @@ app.use("/service/youtube", youtubeRoutes);
 app.use("/service/prime", primeRoutes);
 app.use("/service/hbo", hboRoutes);
 app.use("/service/disney", disneyRoutes);
+
+//User Sign up / Log in Handler
+app.post("/login", (Request, Response) => {
+  const { email, password } = Request.body;
+});
+app.post("/signup", (Request, Response) => {
+  const {
+    firstName,
+    lastName,
+    province,
+    city,
+    zip,
+    address,
+    email,
+    password,
+    contact,
+  } = Request.body;
+});
+
+// Wallets Handler
+app.post("/getWallets", (Request, Response) => {});
+app.post("/addWallet", (Request, Response) => {});
+app.post("/removeWallet", (Request, Response) => {});
+
+//Subscriptions
+app.post("/getSubscriptions", (Request, Response) => {});
+app.post("/addSubscription", (Request, Response) => {});
+app.post("/removeSubscription", (Request, Response) => {});
 
 app.listen(5000, () => console.log("Server running at PORT 5000"));
