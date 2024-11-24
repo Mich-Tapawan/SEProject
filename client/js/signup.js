81e49856a3df7462b83f2d4489dee3a613a1dedb
Mich-Tapawan <mik.tapawan@gmail.com>
2024-11-25 00:50:23 +0800

    Added content for sign up payment details

--- client/js/signup.js
+++ client/js/signup.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const signUpSection = document.getElementById("signup-form");
+  const paymentMethodSection = document.getElementById("payment-method");
+
   document.querySelector("#cancel").addEventListener("click", () => {
     event.preventDefault();
     window.location.href = "/client/index.html";
   });
 
-  document.querySelector("#next").addEventListener("click", () => {});
+  document.querySelector("#next").addEventListener("click", () => {
+    signUpSection.style.display = "none";
+    paymentMethodSection.style.display = "block";
+  });
 });
