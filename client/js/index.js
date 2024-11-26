8f10fa81c7502c62ba3d8330e74685465f720fab
Mich-Tapawan <mik.tapawan@gmail.com>
2024-11-26 17:53:39 +0800

    Added instruction comments

--- client/js/index.js
+++ client/js/index.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const loginEmail = document.getElementById("login-email");
   const loginPassword = document.getElementById("login-password");
+
   document.querySelector("#login").addEventListener("click", () => {
     event.preventDefault();
-    if (loginEmail.value != "" && loginPassword.value != "") {
+    if (loginEmail.value == "" && loginPassword.value == "") {
+      //Dito kayo mag edit ng alert code
+    } else {
       window.location.href = "/client/pages/home.html";
     }
   });
