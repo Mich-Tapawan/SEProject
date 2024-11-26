10f0da270ca36a2f2308e07f724cd102cce012ad
Mich-Tapawan <mik.tapawan@gmail.com>
2024-11-26 21:03:40 +0800

    Integrated dynamic values for user information

--- client/js/account.js
+++ client/js/account.js
@@ -46,4 +46,24 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     };
   });
+
+  // Integrating user data
+  const userName = document.getElementById("name");
+  const userCountry = document.getElementById("country");
+  const userState = document.getElementById("state");
+  const userCity = document.getElementById("city");
+  const userAddress = document.getElementById("address");
+  const userEmail = document.getElementById("email");
+  const userPhone = document.getElementById("phone");
+  const item = localStorage.getItem("userData");
+  const user = JSON.parse(item);
+  const userData = user.user;
+
+  userName.innerHTML = `${userData.firstName} ${userData.lastName}`;
+  userCountry.innerHTML = userData.country;
+  userState.innerHTML = userData.state;
+  userCity.innerHTML = userData.city;
+  userAddress.innerHTML = userData.address;
+  userEmail.innerHTML = userData.email;
+  userPhone.innerHTML = userData.contact;
 });
